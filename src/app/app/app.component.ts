8d6babe06016c9828fffe92c0034a1c451b510bf
philipp allgeuer <philipp.allgeuer.at@gmail.com>
2016-04-24 13:22:30 +0200

    play with Observable

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,7 +1,29 @@
 import {Component} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'my-app',
   template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent {}
+export class AppComponent {
+
+  private data: Observable<Array<any>>;
+
+  constructor(){
+      this.data = new Observable(observer => {
+        setTimeout(() => {
+          observer.next(42);
+          observer.next(43);
+          observer.complete();
+        }, 2000);
+
+        console.log('Started Observable Seq!');
+      });
+
+      let subscription = this.data.subscribe(
+        value => console.log('Value: ' + value),
+        error => console.error(error),
+        () => console.log('Completed Observable Seq!')
+      );
+  }
+}
