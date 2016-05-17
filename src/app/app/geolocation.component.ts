a4260e6441d6978f1ed05acf2c0bdaf54af295a6
philipp allgeuer <philipp.allgeuer.at@gmail.com>
2016-05-17 23:26:27 +0200

    add error handling

--- src/app/app/geolocation.component.ts
+++ src/app/app/geolocation.component.ts
@@ -6,20 +6,29 @@ import { GeoLocation } from './geolocation.interface';
   selector: 'my-geolocation',
   providers: [geoLocationService],
   template: `
-            <div *ngIf="geoloc">
-              <p>Current lat: {{ geoloc.lat }}</p>
-              <p>Current lon: {{ geoloc.lon }}</p>
+            <div *ngIf="geoloc && !error">
+              <p>Current lat: {{ geoloc.lat | number:'2.1-6' }}</p>
+              <p>Current lon: {{ geoloc.lon | number:'2.1-6' }}</p>
+            </div>
+            <div *ngIf="!geoloc && !error">
+              <p>scanning....</p>
+            </div>
+            <div *ngIf="error">
+              <p>{{ error }}</p>
             </div>
             `
 })
 export class GeolocationComponent implements OnInit {
 
   geoloc: GeoLocation;
+  error: string;
 
   constructor(private _location: geoLocationService) {}
 
   getGeoLoc() {
-    this._location.getGeoLoc().subscribe(geoloc => this.geoloc = geoloc);
+    this._location.getGeoLoc().subscribe(geoloc => this.geoloc = geoloc,
+      error => this.error = error,
+      () => console.info('geoloc scanning done'));
   }
 
   ngOnInit() {
