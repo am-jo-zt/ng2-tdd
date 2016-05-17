2d3a71f9b6c90435cd0f7f8971bda92c8de5e8f8
philipp.allgeuer.at@gmail.com <philipp.allgeuer.at@gmail.com>
2016-05-18 00:21:53 +0200

    use styles propertie

--- src/app/app/geolocation.component.ts
+++ src/app/app/geolocation.component.ts
@@ -5,18 +5,22 @@ import { GeoLocation } from './geolocation.interface';
 @Component({
   selector: 'my-geolocation',
   providers: [geoLocationService],
+  //TODO: use a html file
   template: `
-            <div *ngIf="geoloc && !error">
-              <p>Current lat: {{ geoloc.lat | number:'2.1-6' }}</p>
-              <p>Current lon: {{ geoloc.lon | number:'2.1-6' }}</p>
-            </div>
-            <div *ngIf="!geoloc && !error">
-              <p>scanning....</p>
-            </div>
-            <div *ngIf="error">
-              <p>{{ error }}</p>
-            </div>
-            `
+    <div *ngIf="geoloc && !error" class="alert-box success">
+      <p>lat: {{ geoloc.lat | number:'2.1-6' }} lon: {{ geoloc.lon | number:'2.1-6' }}</p>
+    </div>
+    <div *ngIf="!geoloc && !error" class="alert-box notice">
+      <p>scanning....</p>
+    </div>
+    <div *ngIf="error" class="alert-box error">
+      <p>{{ error }}</p>
+    </div>`,
+  //TODO: use a css file
+  styles: ['.alert-box { border-radius:10px; padding:10px; margin:10px;}',
+    '.notice { border:1px solid; }',
+    '.error { border:1px solid; }',
+    '.success { border:1px solid; }']
 })
 export class GeolocationComponent implements OnInit {
 
