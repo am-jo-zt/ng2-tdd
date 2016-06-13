3584ca2a7465c3d69f1e385eb580426256f4e122
philipp.allgeuer.at@gmail.com <philipp.allgeuer.at@gmail.com>
2016-06-13 02:14:59 +0200

    working version

--- tools/utils/clean.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var del = require('del');
-function clean(path) {
-    return del([path]).then(function (p) { console.log('Delete: ', p.join('')); });
-}
-exports.clean = clean;
-//# sourceMappingURL=clean.js.map
