17f7fe70b1232a0702819a252342905116ebdd4d
philipp.allgeuer.at@gmail.com <philipp.allgeuer.at@gmail.com>
2016-06-15 10:40:53 +0200

    lol with jsa80

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,8 @@ import { TestComponentBuilder } from '@angular/compiler/testing';
 import { Component } from '@angular/core';
 import { AppComponent } from './app.component';
 import {
-  beforeEach,
+  async,
+  inject,
   describe,
   it,
   expect
@@ -13,14 +14,14 @@ main test fuction for app.component
 */
 export function main() {
   describe('App component', () => {
-
-    beforeEach(() => {
-      this.app = new AppComponent();
-    });
-
-    it('should build without a problem', () => {
-      expect(this.app).toBeDefined();
-    });
+    it('should build without a problem',
+      async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+        tcb.createAsync(TestComponent)
+          .then((fixture) => {
+            console.log(fixture.nativeElement.innerText);
+            //expect(fixture.nativeElement.innerText.indexOf('HOME')).toBeTruthy();
+          });
+    })));
   });
 }
 
